26d0b02201dedafb7fb254117205556bb164f904
iván <142864473+gl0w-w@users.noreply.github.com>
2025-05-07 01:32:16 -0400

    Delete Ferremas-api directory

--- Ferremas-api/api-ferremas/src/main/java/com/ferremas/api_ferremas/repository/ProductoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ferremas.api_ferremas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ferremas.api_ferremas.model.Producto;
-
-public interface ProductoRepository extends JpaRepository<Producto, Long> {
-}
--- Ferremas-api/api-ferremas/src/main/java/com/ferremas/api_ferremas/repository/SucursalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ferremas.api_ferremas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ferremas.api_ferremas.model.Sucursal;
-
-public interface SucursalRepository extends JpaRepository<Sucursal, Long> {
-}
